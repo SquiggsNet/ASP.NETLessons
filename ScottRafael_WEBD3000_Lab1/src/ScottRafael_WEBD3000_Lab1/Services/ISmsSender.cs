f1f204de0a12b49185813a3b803d6073deaa32c3
SquiggsNet <squiggs.rafael@gmail.com>
2017-01-09 15:20:36 -0400

    Added new project

---
+++ ScottRafael_WEBD3000_Lab1/src/ScottRafael_WEBD3000_Lab1/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScottRafael_WEBD3000_Lab1.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
