97eadc92cbd6f064bcd06aa31a47367cece1c7e7
SquiggsNet <squiggs.rafael@gmail.com>
2017-01-12 10:48:58 -0400

    Chapter 3 completed

--- ScottRafael_WEBD3000_Lab1/src/ScottRafael_WEBD3000_Lab1/Controllers/HomeController.cs
+++ ScottRafael_WEBD3000_Lab1/src/ScottRafael_WEBD3000_Lab1/Controllers/HomeController.cs
@@ -1,23 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScottRafael_WEBD3000_Lab1.Data;
+using ScottRafael_WEBD3000_Lab1.Models.SchoolViewModels;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace ScottRafael_WEBD3000_Lab1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
---
+++ ScottRafael_WEBD3000_Lab1/src/ScottRafael_WEBD3000_Lab1/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ScottRafael_WEBD3000_Lab1.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
