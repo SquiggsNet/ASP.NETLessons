c28b3e5716f975f9d9c4497d46abb2eeb356064e
SquiggsNet <squiggs.rafael@gmail.com>
2017-01-16 21:10:45 -0400

    Chapter 8 complete

--- ScottRafael_WEBD3000_Lab1/src/ScottRafael_WEBD3000_Lab1/Models/Department.cs
+++ ScottRafael_WEBD3000_Lab1/src/ScottRafael_WEBD3000_Lab1/Models/Department.cs
@@ -23,6 +23,9 @@ namespace ScottRafael_WEBD3000_Lab1.Models
 
         public int? InstructorID { get; set; }
 
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
         public Instructor Administrator { get; set; }
         public ICollection<Course> Courses { get; set; }
     }
