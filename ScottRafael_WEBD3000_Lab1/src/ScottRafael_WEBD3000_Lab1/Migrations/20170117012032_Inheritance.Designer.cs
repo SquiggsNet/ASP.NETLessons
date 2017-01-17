2e1f5fea4258895773914e1dad8498efccab340c
SquiggsNet <squiggs.rafael@gmail.com>
2017-01-16 21:30:20 -0400

    Chapter 9 Complete

--- ScottRafael_WEBD3000_Lab1/src/ScottRafael_WEBD3000_Lab1/Migrations/20170117012032_Inheritance.Designer.cs
+++ ScottRafael_WEBD3000_Lab1/src/ScottRafael_WEBD3000_Lab1/Migrations/20170117012032_Inheritance.Designer.cs
@@ -8,9 +8,10 @@ using ScottRafael_WEBD3000_Lab1.Data;
 namespace ScottRafael_WEBD3000_Lab1.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20170117012032_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
