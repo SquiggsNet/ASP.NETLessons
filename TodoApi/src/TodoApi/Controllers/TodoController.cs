e115d935f33c7ae784ffc861cf3a1dd00f951490
SquiggsNet <squiggs.rafael@gmail.com>
2017-03-15 14:13:40 -0300

    Lab2 Complete

--- TodoApi/src/TodoApi/Controllers/TodoController.cs
+++ TodoApi/src/TodoApi/Controllers/TodoController.cs
@@ -1,24 +1,46 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TodoApi.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace TodoApi.Controllers
 {
+    /// <summary>
+    /// API Controller for Todo Items
+    /// </summary>
     [Route("api/[controller]")]
     public class TodoController : Controller
     {
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="todoItems"></param>
         public TodoController(ITodoRepository todoItems)
         {
             TodoItems = todoItems;
         }
+
+        /// <summary>
+        /// repository for Todo items
+        /// </summary>
         public ITodoRepository TodoItems { get; set; }
 
+        /// <summary>
+        /// returns a collection of TodoItems.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IEnumerable<TodoItem> GetAll()
         {
             return TodoItems.GetAll();
         }
 
+        /// <summary>
+        /// Returns a specific TodoItem. 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}", Name = "GetTodo")]
         public IActionResult GetById(string id)
         {
@@ -30,7 +52,27 @@ namespace TodoApi.Controllers
             return new ObjectResult(item);
         }
 
+        /// <summary>
+        /// Creates a TodoItem.
+        /// </summary>
+        /// <remarks>
+        /// Note that the key is a GUID and not an integer.
+        ///  
+        ///     POST /Todo
+        ///     {
+        ///        "key": "0e7ad584-7788-4ab1-95a6-ca0a5b444cbb",
+        ///        "name": "Item1",
+        ///        "isComplete": true
+        ///     }
+        /// 
+        /// </remarks>
+        /// <param name="item"></param>
+        /// <returns>New Created Todo Item</returns>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>
         [HttpPost]
+        [ProducesResponseType(typeof(TodoItem), 201)]
+        [ProducesResponseType(typeof(TodoItem), 400)]
         public IActionResult Create([FromBody] TodoItem item)
         {
             if (item == null)
@@ -41,6 +83,15 @@ namespace TodoApi.Controllers
             return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
         }
 
+        /// <summary>
+        /// Updates a specific TodoItem.
+        /// </summary>
+        /// <remarks>
+        /// This is just some additional information that you can put in regarding the method.
+        /// </remarks>
+        /// <param name="id"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         public IActionResult Update(string id, [FromBody] TodoItem item)
         {
@@ -59,6 +110,15 @@ namespace TodoApi.Controllers
             return new NoContentResult();
         }
 
+        /// <summary>
+        /// Updates a specific TodoItem.
+        /// </summary>
+        /// <remarks>
+        /// This is just some additional information that you can put in regarding the method.
+        /// </remarks>
+        /// <param name="id"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
         [HttpPatch("{id}")]
         public IActionResult Update([FromBody] TodoItem item, string id)
         {
@@ -79,6 +139,10 @@ namespace TodoApi.Controllers
             return new NoContentResult();
         }
 
+        /// <summary>
+        /// Deletes a specific TodoItem.
+        /// </summary>
+        /// <param name="id"></param>
         [HttpDelete("{id}")]
         public IActionResult Delete(string id)
         {
--- TodoApi/src/TodoApi/Startup.cs
+++ TodoApi/src/TodoApi/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using TodoApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
 
 namespace TodoApi
 {
@@ -40,7 +43,29 @@ namespace TodoApi
 
             services.AddMvc();
 
+            services.AddLogging();
+
+            // Add our repository type
             services.AddSingleton<ITodoRepository, TodoRepository>();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact { Name = "Scott Rafael", Email = "squiggs.rafael@gmail.com", Url = "http://squiggs.net" },
+                    License = new License { Name = "Use under LICX", Url = "http://url.com" }
+                });
+
+                //Set the comments path for the swagger json and ui.
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "TodoApi.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
 
         }
 
@@ -55,6 +80,19 @@ namespace TodoApi
             app.UseApplicationInsightsExceptionTelemetry();
 
             app.UseMvc();
+
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
